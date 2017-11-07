e5e322820c5fabd41aed16cbf88cd5786a19a651
jasperdenduijf <31652548+jasperdenduijf@users.noreply.github.com>
2017-11-07 12:44:15 +0100

    Update wijk1.py

--- wijk1.py
+++ wijk1.py
@@ -14,16 +14,59 @@ houseimg = OffsetImage(house, zoom=.1)
 # get the house coordinates
 xy = []
 
+# create classes
+class House:
+
+	def __init__(self, x, y, voltage):
+		self.x = x
+		self.y = y
+		self.voltage = voltage
+		self.battery_no = None
+
+	def add_battery(self, battery_no):
+		self.battery_no = battery_no	
+
+class Battery:
+
+	def __init__(self, x, y, voltage):	
+		self.x = x
+		self.y = y
+		self.min_x = x
+		self.min_y = y
+		self.voltage = voltage
+		self.spare_voltage = voltage
+		
 # get the house voltage output next to coordinates
 xyvolt= []
 
+# store houses
 with open('wijk1_huizen.csv', 'rb') as csvfile:
 	reader = csv.reader(csvfile, delimiter = ',')
 	for row in reader:
 		if row[0] != "x":
 			xy.append([int(row[0]), int(row[1]))
 			xyvolt.append([int(row[0]), int(row[1]), int(row[2]))
-		   
+
+# function to creat a new class				       
+def fillHouses(xy_house):
+	new_house = House(xy_house[0], xy_house[1], xy_house[2])
+	return new_house
+
+# create array of houses
+houses = []
+for i in range(0, len(xyvolt)):
+	houses.append(fillHouses(xyvolt[i]))
+	
+#for i in range(0, len(xyvolt)):	
+#	print("Huis {} heeft x = {} en y = {} en volt = {}".format(i, houses[i].x, houses[i].y, houses[i].voltage))
+
+# get data for batteries				       
+raw_battery = []
+
+file =  open('wijk1_batterijen.txt', 'r')
+for line in file: 
+	if(line.split("\t")[0] != "pos"):
+		raw_battery.append([line.split("\t")[0], (line.split("\t")[-1].rstrip())])   
 
 # add the house image to the coordinates
 for row in xy:
