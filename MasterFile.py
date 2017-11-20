bfe5700d4c732e661b4c89ea4f181676ddc1ddeb
11014067 <31650638+11014067@users.noreply.github.com>
2017-11-20 10:58:41 +0100

    hoi

--- MasterFile.py
+++ MasterFile.py
@@ -4,50 +4,18 @@ from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from matplotlib._png import read_png
 from classesWijk import House, Battery
 from SortingAlgorithm import Sorting
+from download_data import download_data
 
 # start the cable length
 cable_length = 0
 
 # DOWNLOAD AND ORDER DATA
-# download the raw house data in a list	
-xyvolt= []
-with open('Wijk_informatie/wijk1_huizen.csv', 'rb') as csvfile:
-	reader = csv.reader(csvfile, delimiter = ',')
-	for row in reader:
-		if row[0] != "x":
-			xyvolt.append([int(row[0]), int(row[1]), float(row[2])])
-
-# stores the data into classes
-def fillHouses(xy_house):
-	new_house = House(xy_house[0], xy_house[1], xy_house[2])
-	return new_house
-
-houses = []
-for i in range(0, len(xyvolt)):
-	houses.append(fillHouses(xyvolt[i]))
-	
-# download the raw battery data in a list		
-raw_battery = []
-file =  open('Wijk_informatie/wijk1_batterijen.txt', 'r')
-for line in file: 
-	if(line.split("\t")[0] != "pos"):
-		list_string = line.split("\t")[0]
-		list_string = list_string[1:len(list_string)-1]
-		raw_battery.append([int(list_string.split(",")[0]), int(list_string.split(",")[1]), float(line.split("\t")[-1].rstrip())])
-
-# stores the data into classes	
-def fillBatteries(data_battery):
-	new_battery = Battery(data_battery[0], data_battery[1], data_battery[2])
-	return new_battery
-
-batteries = []
-for i in range(0, len(raw_battery)):
-	batteries.append(fillBatteries(raw_battery[i]))
-	batteries[i].add_name(i)
-	print("Battery {} on index {} has x = {}".format(batteries[i].name, i, batteries[i].x))
+information = download_data()
+batteries = information[0]
+houses = information[1]
 	
 # ALGORITHM
-information = Sorting(batteries, houses, 'battery.x', 'house.x')
+information = Sorting(batteries, houses, 'x', 'x')
 batteries = information[0]
 houses = information[1]
 	
