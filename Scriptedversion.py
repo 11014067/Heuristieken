ebf03d4a6a4c3296892549d18c9fec5ade3f7761
11014067 <31650638+11014067@users.noreply.github.com>
2017-11-20 10:08:13 +0100

    .

--- Scriptedversion.py
+++ Scriptedversion.py
@@ -3,7 +3,7 @@ import csv
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
 from matplotlib._png import read_png
-from .Start_up import Classes
+from .Start_up import classesWijk
 
 # start the cable length
 cable_length = 0
