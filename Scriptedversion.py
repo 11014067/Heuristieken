a7ba2d770802c61abde237cae1bfb911418c20ad
11014067 <31650638+11014067@users.noreply.github.com>
2017-11-20 10:23:11 +0100

    .

--- Scriptedversion.py
+++ Scriptedversion.py
@@ -8,51 +8,51 @@ from classesWijk import House, Battery
 # start the cable length
 cable_length = 0
 
-## define classes
-#class House:
-#
-#	def __init__(self, x, y, voltage):
-#		self.x = x
-#		self.y = y
-#		self.voltage = voltage
-#		self.battery_no = None
-#
-#	def add_battery(self, battery_no):
-#		self.battery_no = battery_no	
-#
-#class Battery:
-#
-#	def __init__(self, x, y, voltage):	
-#		self.x = x
-#		self.y = y
-#		self.min_x = x
-#		self.max_x = x
-#		self.voltage = voltage
-#		self.spare_voltage = voltage
-#		
-#	def add_name(self, name):
-#		self.name = name
-#	
-#	# checks whether a house can be added and adds if possible
-#	def add_house(self, house):
-#	
-#		if self.spare_voltage > house.voltage:
-#			if self.min_x > house.x:
-#				self.min_x = house.x
-#				house.add_battery(self.name)
-#				self.spare_voltage -= house.voltage
-#				return True
-#			elif self.max_x < house.x:
-#				self.max_x = house.x
-#				house.add_battery(self.name)
-#				self.spare_voltage -= house.voltage
-#				return True
-#			else:
-#				house.add_battery(self.name)
-#				self.spare_voltage -= house.voltage
-#				return True
-#		else:
-#			return False
+# define classes
+class House:
+
+	def __init__(self, x, y, voltage):
+		self.x = x
+		self.y = y
+		self.voltage = voltage
+		self.battery_no = None
+
+	def add_battery(self, battery_no):
+		self.battery_no = battery_no	
+
+class Battery:
+
+	def __init__(self, x, y, voltage):	
+		self.x = x
+		self.y = y
+		self.min_x = x
+		self.max_x = x
+		self.voltage = voltage
+		self.spare_voltage = voltage
+		
+	def add_name(self, name):
+		self.name = name
+	
+	# checks whether a house can be added and adds if possible
+	def add_house(self, house):
+	
+		if self.spare_voltage > house.voltage:
+			if self.min_x > house.x:
+				self.min_x = house.x
+				house.add_battery(self.name)
+				self.spare_voltage -= house.voltage
+				return True
+			elif self.max_x < house.x:
+				self.max_x = house.x
+				house.add_battery(self.name)
+				self.spare_voltage -= house.voltage
+				return True
+			else:
+				house.add_battery(self.name)
+				self.spare_voltage -= house.voltage
+				return True
+		else:
+			return False
 
 # HOUSE PART
 # download the raw house data in a list			
