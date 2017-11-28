9feb2397220d4010033b3e7dcbf7d80cb711e7ff
11014067 <31650638+11014067@users.noreply.github.com>
2017-11-28 23:03:48 +0100

    commented

--- DistanceAlgorithm.py
+++ DistanceAlgorithm.py
@@ -1,19 +1,24 @@
 from switcher import switching_algorithm
 
 def distanceAlgorithm(batteries, houses, sort_battery):
+	# Sort the batteries
 	print "Sorting..."
 	if (sort_battery != "voltage"):
 		batteries = sorted(batteries, key=lambda battery: getattr(battery, sort_battery))
 	else:
 		batteries = sorted(batteries, key=lambda battery: -battery.voltage)
+	
+	# Sort the houses on id
 	houses = sorted(houses, key=lambda house: house.id)
 	
+	# Make a list for each battery
 	distance0list = []
 	distance1list = []
 	distance2list = []
 	distance3list = []
 	distance4list = []
 	
+	# Put the distance from each house to each battery in the list as [distance, house id]
 	for house in houses:
 		distance0 = abs(house.x - batteries[0].x) + abs(house.y - batteries[0].y)
 		distance1 = abs(house.x - batteries[1].x) + abs(house.y - batteries[1].y)
@@ -26,41 +31,40 @@ def distanceAlgorithm(batteries, houses, sort_battery):
 		distance3list.append([distance3, house.id])
 		distance4list.append([distance4, house.id])
 	
-	def numeric_compare(x, y):
-		return x - y
-		
+	# Sort the list on distance	
 	distance0list = sorted(distance0list, key=lambda x: x[0])
 	distance1list = sorted(distance1list, key=lambda x: x[0])
 	distance2list = sorted(distance2list, key=lambda x: x[0])
 	distance3list = sorted(distance3list, key=lambda x: x[0])
 	distance4list = sorted(distance4list, key=lambda x: x[0])
 	
+	# Put all the lists together for easy access
 	distanceslists = [distance0list, distance1list, distance2list, distance3list, distance4list]
 	
-	b = [0,0,0,0,0]
-	placednum = 0
+	# Save the indexes and how many houses are placed
+	index_list = [0,0,0,0,0]
+	houses_to_place = houses.length
 	
 	# place all the houses
-	while placednum < 150:
-		# pass the batteries
-		j = 0
-		for battery in batteries:
+	while houses_to_place > 0:
+		for (var j = 0; j < batteries.length; j++):
 			# only try to add if it has spare voltage
-			if battery.spare_voltage > 0:
+			if batteries[j].spare_voltage > 0:
 				# place the first posible house
 				placeHouse = False
 				while placeHouse == False:
-					if b[j] == 150:
-						b[j] = 149
+					if index_list[j] == 150:
+						index_list[j] = 149
+						# if there are no houses left to place, go to the next battery
 						placeHouse = True
-					elif houses[distanceslists[j][b[j]][1]].placed == False:
-						if battery.add_house(houses[distanceslists[j][b[j]][1]]):
-							#house placed op true
-							placednum += 1
+					elif houses[distanceslists[j][index_list[j]][1]].placed == False:
+						if batteries[j].add_house(houses[distanceslists[j][index_list[j]][1]]):
+							# house placed op true
+							houses_to_place -= 1
+							# go to the next battery
 							placeHouse = True
-					b[j] += 1
-			j += 1
-		if b[0] == 150 and b[1] == 150 and b[2] == 150 and b[3] == 150 and b[4] == 150:
+					index_list[j] += 1
+		if index_list[0] == 150 and index_list[1] == 150 and index_list[2] == 150 and index_list[3] == 150 and index_list[4] == 150:
 			print(" UNSUCCESFULL ")
 			break
 			
