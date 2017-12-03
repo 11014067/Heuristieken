6c925097d0bd6ad5bb61bd290fa18d3b517db744
11014067 <31650638+11014067@users.noreply.github.com>
2017-12-03 22:35:35 +0100

    ask nicely is implemented in the rest of the main file

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from download_data import download_data
 from plot_grid import plot_grid as plot_grid
 from ask_nicely import ask_nicely
 
-ask_nicely()
+# remember the method to use, return index[1] not in use
+method = ask_nicely()
 
 # choose the neighborhood number
-wijk = '1'
+wijk = method[0]
 
 # choose the sorting method for batteries and houses (x, y, voltage, random or distance)
-battery_sort = 'x'
-house_sort = 'x'
+battery_sort = method[2]
+house_sort = method[3]
 
 # choose the battery size, standaard bij wijk 1 1507, wijk 2 1508.25 en wijk 3 1506.75
 battery_size = [1507, 1507, 1507, 1507, 1507]
