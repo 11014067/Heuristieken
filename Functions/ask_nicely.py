acf9d3e844a7b5088c1c4807c567151e140ed383
jasperdenduijf <31652548+jasperdenduijf@users.noreply.github.com>
2017-12-11 12:48:20 +0100

    variable questions

--- Functions/ask_nicely.py
+++ Functions/ask_nicely.py
@@ -2,14 +2,22 @@ def ask_nicely():
 
 	# create boolians 
 	check1 = True
+	check1b = True
+	check1c = True
+	check1d = True
 	check2 = True
 	check3 = True
 	check4 = True
 
 	# create possible correct answers for the questions
 	neighborhood_list = ["1", "2", "3"]
+	variable_batteries_list = ["y", "n", "yes", "no"]
 	sorting_method_list = ["s", "d", "sorting", "distance"]
 	sorting_options = ["x", "y", "voltage", "random", "v", "r"]
+	
+	variable_batteries_voltages = []
+	variable_batteries_bool = False
+	
 	
 	# ask for the neighborhood
 	print ("You can pick neighborhood 1, 2 or 3")
@@ -21,6 +29,36 @@ def ask_nicely():
 		else:
 			print ("Please choose 1, 2 or 3")
 	
+	# ask for the algorithm
+	print ("Do you want to make batteries variable? Yes/no:")
+	while check1b:
+		variable_batteries = input("")
+		if variable_batteries in variable_batteries_list:
+			if variable_batteries.index(variable_batteries) < 2:
+				variable_batteries = variable_batteries_list[variable_batteries_list.index(variable_batteries) + 2]
+			print ("You entered", variable_batteries)
+			check1b = False
+		else:
+			print ("Please answer with yes or no:")
+			
+	if variable_batteries == "yes":
+		variable_batteries_bool = True
+		while check1c:
+			print ("How many batteries do you want?")
+			number_of_batteries = input("")
+			if(int(number_of_batteries) in range(1, 31)):
+				check1c = False
+		for i in range(0, int(number_of_batteries)):
+			new_entry = True
+			while new_entry:
+				voltage_battery = input("How much voltage has battery " + str(i) + ": ")
+				if int(voltage_battery) > 0 and int(voltage_battery) <= 1800:
+					variable_batteries_voltages.append(int(voltage_battery))
+					new_entry = False
+				else:
+					print("Please enter a number between 0 and 1800")
+		print(variable_batteries_voltages)
+		
 	# ask for the algorithm
 	print ("You can currently use 2 algorithms: sorting or distance")
 	while check2:
@@ -60,6 +98,7 @@ def ask_nicely():
 				print ("Please choose x, y, voltage or random")
 	else:
 		sorting_houses = None
+		
 	
-	return [neighborhood, sorting_method, sorting_batteries, sorting_houses]
+	return [neighborhood, sorting_method, sorting_batteries, sorting_houses, variable_batteries_bool, variable_batteries_voltages]
 	
