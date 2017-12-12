84b6eb4078269c70f375129a21b6419c92608cb6
DaphneBox2 <daphne.box@student.uva.nl>
2017-12-12 15:10:13 +0100

    start hill climber

---
+++ Functions/hill_climber.py
@@ -0,0 +1,8 @@
+from switcher import switching_algorithm
+from Classes.neighborhood_classes import House, Battery
+from Functions.download_data import download_data
+from Functions.score_function import score_function
+
+# select random battery
+select_battery = select(random(battery_no))
+select_house = select(random(house.select_battery)
