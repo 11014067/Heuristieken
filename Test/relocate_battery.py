d15c09886b086ce0cf7714b060bf4e22277ff667
11014067 <31650638+11014067@users.noreply.github.com>
2017-12-18 21:00:11 +0100

    i

--- Test/relocate_battery.py
+++ Test/relocate_battery.py
@@ -4,7 +4,7 @@ from Algorithms.distance_algorithm import distance_algorithm
 from Classes.neighborhood_classes import Neighborhood_class
 from Functions.plot_grid import plot_grid
 from Functions.score_function import score_function
-from Functions.switcher import switcher
+from Functions.switcher import switching_algorithm
 from Test.replace_batteries import replace
 from Test.empty import empty
 import time
