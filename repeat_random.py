dbe85b46f5ee56454a48bab1dc099105c944f5ba
11014067 <31650638+11014067@users.noreply.github.com>
2017-12-18 20:22:01 +0100

    all is well

--- repeat_random.py
+++ repeat_random.py
@@ -1,50 +1,42 @@
+# This function repeats the main with a certain input.
+
 from main import main
 from Classes.neighborhood_classes import Neighborhood_class
 import sys
 import time
 
+# start a timer
 t0 = time.time()
 
+improvement_rate = []
 best_solution = Neighborhood_class("long")
 best_solution.cable_length = 9999999999
-print(best_solution.cable_length)
 
-improvement_rate = []
-all_solutions = []
-
+# stores the last system path
 f1 = sys.stdin
-for i in range (0, 30):
-	print("Dit is het " + str(i) + "e couplet.")
-	    
+
+# start 150000 iterations
+for i in range (0, 150000):
+	
+	# opens a text file with the commands
 	f = open('input.txt','r')
 	sys.stdin = f
 	solution = main(a = "a")
 	f.close()
-
-	all_solutions.append([i,solution.cable_length])
-		
+	
+	# check if you found a better cable length
 	if solution.cable_length < best_solution.cable_length:
 		best_solution = solution
-
 		improvement_rate.append([i, best_solution.cable_length])
 		
+# restore the last system path
 sys.stdin = f1	
 
+# stop timer
 t1 = time.time()
 
-print("The final cable_length of each iteration is: {}".format(all_solutions))
-
-print("En de winnaar heeft kabellengte:")	
-print(best_solution.cable_length)
-
-print("Check this!")
-print(improvement_rate)
-
-very_long_string = ""
-for house in best_solution.houses:
-	very_long_string += "House " + str(house.id) + " on (" + str(house.x) + "," + str(house.y) + ") with voltage " + str(house.voltage) + " connected to battery " + str(house.battery_no) + ". \n" 
-print(very_long_string)	
-
-print("Running time:", t1-t0)
-
-	
+# print the results
+print("Best cable length: ", best_solution.cable_length)
+print("Improvement rate: ", improvement_rate)
+best_solution.show_houses()
+print("Running time:", t1-t0)	
